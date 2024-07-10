0de1ac6d6c0504a78757f2c6234c4482a5a63192
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2024-07-10 16:29:32 +0400

    update

---
+++ Application/Models/Configuration/DataBaseConfiguration/StoreConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Configuration.DataBaseConfiguration
+{
+    public class StoreConfiguration : IEntityTypeConfiguration<Store>
+    {
+        public void Configure(EntityTypeBuilder<Store> builder)
+        {
+
+            builder.Property(x => x.Id)
+                .IsRequired();
+
+            builder.Property(x=>x.Addresses)
+                .IsRequired();
+            
+
+        }
+
+    }
+}
